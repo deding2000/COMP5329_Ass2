b35bd417d2d506052fe801cfed39f90b0b39ac76
deding2000 <chr2830@gmail.com>
2025-05-04 18:12:47 +1000

    more dataloading

--- Dataload.py
+++ Dataload.py
@@ -1,11 +1,17 @@
 import re
 import pandas as pd
 from io import StringIO
+import os
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 import math
 import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.io import read_image
 
 # Set device to GPU if this is available
 use_cuda = True if torch.cuda.is_available() else False
@@ -13,15 +19,19 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('We are using GPU.' if use_cuda else 'We are using CPU.')
 
 # Loading filenames and labels
-dataset_path = "Dataset/"
+# Assumes data is in folder "Dataset"
+DATASET_PATH = "Dataset/"
 FILENAME_TRAIN = 'train.csv'
 FILENAME_TEST = 'test.csv'
-with open(dataset_path + FILENAME_TRAIN) as file:
+
+with open(DATASET_PATH + FILENAME_TRAIN) as file:
     lines = [re.sub(r'([^,])"(\s*[^\n])', r'\1/"\2', line) for line in file]
     df_train = pd.read_csv(StringIO(''.join(lines)), escapechar="/")
-with open(dataset_path + FILENAME_TEST) as file:
+with open(DATASET_PATH + FILENAME_TEST) as file:
     lines = [re.sub(r'([^,])"(\s*[^\n])', r'\1/"\2', line) for line in file]
     df_test = pd.read_csv(StringIO(''.join(lines)), escapechar="/")
+df_train.to_csv("train_labels_captions.csv")
+df_test.to_csv("test_labels_captions.csv")
 
 # To get overview
 #print(df_train.describe)
@@ -32,12 +42,56 @@ def pil_process_image_color(image):
   arr = np.asarray(image)
   return arr
 
-image_path = dataset_path + "data/0.jpg"
+# Testing loading
+image_path = DATASET_PATH + "data/100.jpg"
 image = pil_process_image_color(image_path)
-print(image.shape)
+#print(np.max(image)) # images are in range (0,255) with 3 channels
+#print(image.dtype) # dtype is uint8
 
+# Defining transforms
+INPUT_HEIGHT = 128
+INPUT_WIDTH = 128
+resize = transforms.Resize(size=(INPUT_HEIGHT,
+        INPUT_WIDTH)) # Trying to resize to (INPUT_HEIGHT,INPUT_WIDTH)
 
+# TODO: we can add data augmentation here
+# See for example https://pyimagesearch.com/2021/10/04/image-data-loaders-in-pytorch/
+trainTransforms = transforms.Compose([resize]) 
+testTransforms = transforms.Compose([resize]) 
 
+# Custom Dataset class for our data
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = annotations_file
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
+    def __len__(self):
+        return len(self.img_labels)
 
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
 
+# Create dataloaders
+training_data = CustomImageDataset(df_train,DATASET_PATH+"data/",transform=trainTransforms)
+test_data = CustomImageDataset(df_test,DATASET_PATH+"data/",transform=testTransforms)
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Display image and label.
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch size: {len(train_labels)}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img.permute(1, 2, 0), cmap="gray")
+plt.show()
+print(f"Label: {label}")
